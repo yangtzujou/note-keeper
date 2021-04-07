4537f90141fa48ec524dd8c8c9d751dd4111f8d6
yangtzujou <yangtzujou@gmail.com>
2021-04-07 16:39:20 +0800

    first commit

---
+++ build/precache-manifest.87e6d61d0fe3f3288d042b6fde073068.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb02d488870680c6924621729618b85",
+    "url": "/note-keeper/index.html"
+  },
+  {
+    "revision": "1bbc4f431a1adf9f4ede",
+    "url": "/note-keeper/static/js/2.05aaf037.chunk.js"
+  },
+  {
+    "revision": "00274118936b2db18a65",
+    "url": "/note-keeper/static/js/main.a5c5904b.chunk.js"
+  },
+  {
+    "revision": "56b43f94fa31fc215a75",
+    "url": "/note-keeper/static/js/runtime-main.0100c48c.js"
+  }
+]);
